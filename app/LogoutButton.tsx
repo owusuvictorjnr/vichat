f6280aeb3b691ca316ab4031ec4146d99bd89485
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-24 15:21:07 -0500

    Featured: clear the import react and replce it with use client

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+'use client';
 
 const LogoutButton = () => {
   return (
-    <button className="capitalize bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button
+      className="capitalize bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={() => console.log('Hello')}
+    >
       sign out
     </button>
   );
