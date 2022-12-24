02133445407e934242be5a4deb1526bee7cef8f7
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-24 13:11:03 -0500

    Featured: Sign out button

---
+++ app/LogoutButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogoutButton = () => {
+  return (
+    <button className="capitalize bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      sign out
+    </button>
+  );
+};
+
+export default LogoutButton;
