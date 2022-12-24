3bb7f7fde7a698184e8c85bd36d6fb1992610751
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-24 13:12:18 -0500

    Featured: Main Body of the app

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Anything in this appDir is a server side
+const HomePage = () => {
+  return (
+    <div className="text-xl text-red-600">
+      <h1>Welcome to Vichat Messenger</h1>
+    </div>
+  );
+};
+
+export default HomePage;
