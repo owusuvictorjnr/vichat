6782183d1d786ebb77754e132cf002d3df3f0d39
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-24 15:22:51 -0500

    Featured: wrap main around MessageList and ChatList

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import ChatInput from './ChatInput';
+import MessageList from './MessageList';
 
-// Anything in this appDir is a server side
+// Anything in this appDir is a server side and nw our index.js
 const HomePage = () => {
   return (
-    <div className="text-xl text-red-600">
-      <h1>Welcome to Vichat Messenger</h1>
-    </div>
+    <main>
+      {/* Message List */}
+
+      <MessageList />
+
+      {/* ChatInput */}
+
+      <ChatInput />
+    </main>
   );
 };
 
